3c9f938f431d7dfed8ebded1b0adfca538f549aa
rameshc <ramesh.madem@tarantula.net>
2023-08-13 18:51:21 +0530

    01

---
+++ react-app01/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+import Message from "../Message";
+
+function ListGroup() {
+  const items = ["New York", "London", "Paris"];
+
+  return (
+    <>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
