1497c7c74b88644b53f5ff18461becc34e70f12d
Ramesh <ramesh.mc@gmail.com>
2023-08-14 14:18:09 +0000

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
