cb5eee65bb3a40de872d505fb9aabc6af1af58cd
Ramesh <Ramesh@RAMESH-PC>
2023-08-16 08:57:17 +0530

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders hello world text", () => {
   render(<App />);
   expect(screen.getByText(/Hello World/i)).toBeInTheDocument();
-  expect(screen.getByText(/Hello World/i)).toBeInTheDocument();
+});
+
+test("renders 7 cards", () => {
+  render(<App />);
+  expect(screen.getAllByText(/card title/i)).toHaveLength(7);
+});
+
+test("renders 7 card descriptions", () => {
+  render(<App />);
+  expect(screen.getAllByText(/card description/i)).toHaveLength(7);
 });
