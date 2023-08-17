8b4f3aa48834817b653049f78bc2d875a25af5a9
Ramesh <Ramesh@RAMESH-PC>
2023-08-17 06:42:08 +0530

    image compare testing

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,11 +1,18 @@
 const { expect, browser, $ } = require("@wdio/globals");
 
 describe("My application start", () => {
-  it("should load the home page properly", async () => {
+  beforeEach(async () => {
     await browser.url("\\");
     await browser.pause(6000);
-    await browser.saveScreenshot("./test/screenshots/1.png");
+  });
+
+  it("should load the home page properly", async () => {
+    await browser.saveFullPageScreen("fullPage", {});
 
     expect(await browser.getTitle()).toEqual("React App");
+  });
+
+  it("should match to baseline", async () => {
+    await expect(await browser.checkFullPageScreen("fullPage", {})).toEqual(0);
   });
 });
